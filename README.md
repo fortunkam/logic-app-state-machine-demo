4f3fadd377e45b10cf0d1274075ae51c13c0e164
Matthew Fortunka <mafortun@microsoft.com>
2020-08-21 15:32:24 +0100

    added project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # logic-app-state-machine-demo
+
+To build the environment run the script in setup.ps1
+
+You will end up with a resource group containing 3 logic apps, a storage account and a logic app connection to table storage.
+
+When the script is completed you will have 2 urls.  One to start the workflow, the other to advance it through the states.
+
+The logic apps are...
+The Base Workflow is the state machine
+The Advance workflow moves the state machine through its states.
+The Subscribe workflow gets the url used by the Advance workflow.
+
+
+
---
+++ setup.ps1
@@ -0,0 +1,40 @@
+# Create Resource group
+$rg = "workflow-logic-app-demo"
+$loc = "uksouth"
+$storageaccountname = -join ((97..122) | Get-Random -Count 15 | % {[char]$_})
+
+az group create -n $rg --location $loc
+
+# Create storage account
+az storage account create -g $rg -n $storageaccountname
+
+# Get the connection strings 
+$connectionString = $(az storage account show-connection-string -g $rg -n $storageaccountname --query connectionString -o tsv)
+$storageAccountKey = $(az storage account keys list -g $rg -n $storageaccountname --query [0].value -o tsv)
+
+# Create tables
+az storage table create -n "logs" --account-key $storageAccountKey --account-name $storageaccountname
+az storage table create -n "errors" --account-key $storageAccountKey --account-name $storageaccountname
+az storage table create -n "callbackurls" --account-key $storageAccountKey --account-name $storageaccountname
+
+# Create a logic app connection
+
+az deployment group create --resource-group $rg `
+    --template-file .\arm\tableconnection.json `
+    --parameters tableAccountName=$storageaccountname tableAccountKey=$storageAccountKey
+
+# Deploy Subscribe Logic App 
+
+$subscribeUrl = $(az deployment group create --resource-group $rg --template-file .\arm\SubscribeToWebhookLogicApp.json --query properties.outputs.logicAppUrl.value -o tsv)
+
+# Deploy the other logic apps
+
+$advanceUrl = $(az deployment group create --resource-group $rg --template-file .\arm\AdvanceWorkflowLogicApp.json --query properties.outputs.logicAppUrl.value -o tsv)
+
+$encodedSubscribeUrl = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($subscribeUrl))
+$workflowUrl = $(az deployment group create --resource-group $rg --template-file .\arm\BaseWorkflowLogicApp.json --parameters subscriberLogicAppUrl="$encodedSubscribeUrl" --query properties.outputs.logicAppUrl.value -o tsv)
+
+Write-Host "Advance Workflow Url"
+Write-Host $advanceUrl
+Write-Host "Base or Start Workflow Url"
+Write-Host $workflowUrl
